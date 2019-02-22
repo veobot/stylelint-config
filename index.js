2fe6f64999228a4f76a87d8683d365fa60fe7a65
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2019-02-21 23:33:12 -0500

    Fixes misleading no-empty-source rule for Vue SFCs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: 'stylelint-config-standard',
   processors: [ 'stylelint-processor-html' ],
+  rules: {
+    'no-empty-source': null, // Allows for .vue files to omit <style> tags without errors
+  },
 };
